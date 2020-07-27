04a0a303cef4a29be2eead974d4c563fb6091bb6
januwA <ajanuw1995@gmail.com>
2020-07-27 10:35:11 +0800

    update packages version

--- test.js
+++ test.js
@@ -4,3 +4,11 @@ console.log(d.inHours); // 24
 
 let d2 = d.add(new Duration({ hours: 3, seconds: 10 }));
 console.log(d2.toString()); /// 27:00:10.000000
+
+let date1 = new Duration({ minutes: 19, seconds: 20 })
+
+// 毫秒
+console.log(date1.inMilliseconds);
+
+// 微秒
+console.log(date1.inMicroseconds);
